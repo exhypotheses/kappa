9d36259c9c2462990df99ec687f726fb0ba84a36
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-08-08 15:55:28 +0100

    Add error/assets/js/ptc.js

---
+++ error/assets/js/ptc.js
@@ -0,0 +1,130 @@
+var Highcharts;
+var url = document.getElementById("ptc").getAttribute("url");
+
+jQuery.getJSON(url, function (calculations){
+
+    // The area under the precision/tpr curve
+    var j = calculations.length - 1;
+    var auc = (calculations[j].data[0].ptc).toFixed(2);
+    
+
+    // Get TPR & FPR series identifiers
+    for (var i = 0; i < (calculations.length - 1); i += 1) {
+        if (calculations[i].name.match("precision")) {
+            var ordinates = i;
+        }
+        if (calculations[i].name.match("sensitivity")) {
+            var abscissae = i;
+        }
+    }
+
+
+    // The graph data
+    var ptc = [];
+    for (var i = 0; i < calculations[ordinates].data.length; i += 1) {
+        ptc.push({
+            x: calculations[abscissae].data[i].y,   // sensitivity, i.e., tpr
+            y: calculations[ordinates].data[i].y,   // precision
+            name: (calculations[ordinates].data[i].x).toFixed(2)   // the threshold value
+        });
+    }
+
+
+    // Options
+    Highcharts.setOptions({
+        lang: {
+            thousandsSep: ","
+        }
+    });
+
+
+    // Graphing
+    Highcharts.chart("container", {
+
+        chart: {
+            type: "line",
+            zoomType: "xy",
+            marginTop: 95
+        },
+
+        title: {
+            text: ''
+        },
+        subtitle: {
+            text: 'Precision/TPR Curve<br>(AUC: ' + auc + ')',
+            x: 25,
+            y: 35
+        },
+
+        credits: {
+            enabled: false
+        },
+
+        legend: {
+            enabled: false,
+            x: 50
+        },
+
+        xAxis: {
+            title: {
+                text: calculations[abscissae].name + "<br> true positive rate"
+            },
+            maxPadding: 0.05,
+            gridLineWidth: 1
+        },
+
+        yAxis: {
+            title: {
+                text: calculations[ordinates].name + "<br> positive predictive value"
+            },
+            maxPadding: 0.05,
+            tickInterval: 0.25,
+            // min: 0,
+            // max: 1,
+            endOnTick: false
+        },
+
+        exporting: {
+            buttons: {
+                contextButton: {
+                    menuItems: ["viewFullscreen", "printChart", "separator",
+                        "downloadPNG", "downloadJPEG", "downloadPDF", "downloadSVG", "separator",
+                        "downloadXLS", "downloadCSV"],
+                    x: 9
+                }
+            }
+        },
+
+        tooltip: {
+            shared: true,
+            headerFormat: '<span style="font-size: 13px; color:{point.color}">\u25CF</span>',
+            pointFormat: ' &nbsp; At threshold: {point.name}<br/><p>' +
+                'True Positive Rate: {point.x:,.2f}<br>Precision: {point.y:,.2f}<br/></p>',
+            style: {
+                fontSize: "11px"
+            }
+        },
+
+        plotOptions: {
+            series: {
+                marker: {
+                    enabled: true,
+                    radius: 1
+                },
+                lineWidth: 1,
+                dataLabels: {
+                    enabled: false
+                },
+                turboThreshold: 4000
+            }
+        },
+
+        series: [{
+            type: "spline",
+            name: "Precision/TPR Curve",
+            data: ptc
+        }]
+
+    });
+
+});
