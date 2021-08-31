ede66be712448858fd3d6f259423056a45aa1c15
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-08-31 22:14:52 +0100

    Update error/assets/js/metrics.js

--- error/assets/js/metrics.js
+++ error/assets/js/metrics.js
@@ -90,7 +90,7 @@ jQuery.getJSON(url, function (calculations){
                 color: "#5D686D",
                 dashStyle: "solid",
                 width: 0.5,
-                value: calculations[j].data[0].optimal,
+                value: calculations[j].data.optimal,
                 label: {
                     rotation: 0,
                     y: 110,  // From the top of the graph and downward
