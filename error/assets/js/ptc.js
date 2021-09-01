a28ad6e8987cacf5023a9a196cbfaedf6f5354c9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-09-01 18:00:24 +0100

    Update error/assets/js/ptc.js

--- error/assets/js/ptc.js
+++ error/assets/js/ptc.js
@@ -44,16 +44,37 @@ jQuery.getJSON(url, function (calculations){
         chart: {
             type: "line",
             zoomType: "xy",
-            marginTop: 95
+            height: 310,
+            width: 290,
+            marginTop: 35,
+            marginBottom: 94,
+            style: {
+                fontSize: '10px',
+                fontWeight: 'bold',
+            }
         },
 
         title: {
             text: ''
         },
-        subtitle: {
+        /* subtitle: {
             text: 'Precision/TPR Curve<br>(AUC: ' + auc + ')',
             x: 25,
             y: 35
+        }, */
+        caption: {
+            text: 'The Precision/TPR Curve.  (AUC: ' + auc + ')<br> &nbsp;',
+            margin: 5,
+            style: {
+                fontStyle: 'italic',
+                fontSize: '11px',
+                fontWeight: 'normal',
+                color: '#9fb7cd',
+                width: '80px'
+            },
+            x: 55,
+            y: 30,
+            floating: true
         },
 
         credits: {
@@ -79,11 +100,11 @@ jQuery.getJSON(url, function (calculations){
                 text: calculations[ordinates].name + "<br> positive predictive value"
             },
             maxPadding: 0.05,
-            tickInterval: 0.25,
-            // min: 0,
-            // max: 1,
+            // tickInterval: 0.25,            
             endOnTick: false,
-            crosshair: true
+            startOnTick: true,
+            crosshair: true,
+            min: 0
         },
 
         annotations: [{
