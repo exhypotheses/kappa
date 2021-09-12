577e6c390ef6172b2be4d82ac6c997d0187baf8f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-09-12 15:10:50 +0100

    Update error/assets/js/tools/latex.js

--- error/assets/js/tools/latex.js
+++ error/assets/js/tools/latex.js
@@ -3,21 +3,12 @@
     http://docs.mathjax.org/en/latest/web/components/index.html#web-components
     http://docs.mathjax.org/en/latest/web/start.html#configuring-mathjax
 */
+
 window.MathJax = {
   tex: {
     inlineMath: [['$', '$'], ['\\(', '\\)']]
-  },
-  svg: {
-    fontCache: 'global'
   }
 };
-
-(function () {
-  var script = document.createElement('script');
-  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
-  script.async = true;
-  document.head.appendChild(script);
-})();
 
 (function () {
   var script = document.createElement('script');
